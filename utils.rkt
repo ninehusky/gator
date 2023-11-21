ed5f7a199173c33eb7387af67302343b38b441db
Andrew Cheung <andrew@andrews-mbp.lan>
2023-11-21 14:49:28 -0800

    Cleanup, LUT refactoring

---
+++ utils.rkt
@@ -0,0 +1,21 @@
+#lang rosette
+
+(require rosette/lib/synthax)
+
+(provide register lift LUT4)
+
+(define-symbolic init (bitvector 32))
+
+(define (register d)
+  (lambda (t) (if (= t 0) init (d (- t 1)))))
+
+(define (lift op . args)
+  (lambda (t) (apply op (map (lambda (stream) (stream t)) args))))
+
+(define (LUT4 a b c d INIT)
+  (lambda (t)
+    (bit 0
+         ((lift bvlshr
+                (lambda (t) INIT)
+                (lambda (t) (zero-extend ((lift concat a b c d) t) (bitvector 16))))
+          t))))
